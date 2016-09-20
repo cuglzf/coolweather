dacf4341cf574b21e94cd91d13d4bca126b35dcd
LZF <liuzhenfang1127@126.com>
2016-09-20 20:56:02 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/cuglzf/coolweather/model/Province.java
@@ -0,0 +1,30 @@
+package com.cuglzf.coolweather.model;
+
+/**
+ * Created by LZF on 2016/9/20.
+ */
+public class Province {
+
+    private int id ;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id ;
+    }
+    public String getProvinceName(){
+        return  provinceName ;
+    }
+    public String getProvinceCode(){
+        return provinceCode ;
+    }
+    public void setId(int id){
+        this.id = id ;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName ;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode ;
+    }
+}
